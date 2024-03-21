6b65f502c7c5caf4c5b1302a4feab6ed8d913ba8
DeCri0t <127325967+DeCri0t@users.noreply.github.com>
2024-03-21 16:56:53 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import { render } from './render.js';
+import { RenderPosition, render } from './render.js';
 import FilterView from './view/filter-view.js';
-import SortView from './view/sort-view.js';
+import TripInfoView from './view/trip-info-view.js';
+import BoardPresenter from './presenter/board-presenter.js';
 
-/* const siteHeader = document.querySelector('.page-header'); */
+const siteHeaderElement = document.querySelector('.trip-main');
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 const siteContentElement = document.querySelector('.trip-events');
 
+const boardPresenter = new BoardPresenter({ container: siteContentElement });
+
+render(new TripInfoView(), siteHeaderElement, RenderPosition.AFTERBEGIN);
 render(new FilterView(), siteFilterElement);
-render(new SortView(), siteContentElement);
+
+boardPresenter.init();
