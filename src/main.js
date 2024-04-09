4f44f3b461593e3c491858aec13ad5044fd7e2bd
юрий <yurapzx@gmail.com>
2024-04-09 20:23:50 +0500

    Разделяй и властвуй

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import { render } from './framework/render.js';
+import FiltersView from './view/filters-view.js';
+import TripEventsPresenter from './presenter/trip-events-presenter.js';
+import SiteMenuView from './view/site-menu-view.js';
+import PointsModel from './model/point-model.js';
+import { getPoints, getDestinations, getOffersByType } from './mock/point.js';
+
+const siteHeaderElement = document.querySelector('.trip-main');
+const siteMainElement = document.querySelector('.page-main');
+const tripPresenter = new TripEventsPresenter(siteMainElement.querySelector('.trip-events'));
+
+const points = getPoints();
+const offersByType = getOffersByType();
+const destinations = getDestinations();
+
+const pointsModel = new PointsModel();
+
+render(new FiltersView(), siteHeaderElement.querySelector('.trip-controls__filters'));
+render(new SiteMenuView(), siteHeaderElement.querySelector('.trip-controls__navigation'));
+
+pointsModel.init(points, destinations, offersByType);
+tripPresenter.init(pointsModel);
