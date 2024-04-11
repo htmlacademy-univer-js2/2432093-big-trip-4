4250ce8f8086859efbbf7c2b7db489dd1234a0c4
DeCri0t <127325967+DeCri0t@users.noreply.github.com>
2024-04-11 11:17:16 +0500

    Update board-presenter.js

--- src/presenter.js/board-presenter.js
+++ src/presenter.js/board-presenter.js
@@ -1,24 +1,76 @@
-import { render } from '../render.js';
-import EditPointView from '../view/edit-point-view.js';
-import EventListView from '../view/event-list-view.js';
-import SortView from '../view/sort-view.js';
-import PointView from '../view/point-view.js';
+import { render, replace } from '../framework/render.js';
+import PointsView from '../view/trip-points-view.js';
+import PreviewPointView from '../view/preview-point-view.js';
+import EditingPointView from '../view/editing-point-view.js';
+import SortingView from '../view/sorting-view.js';
+import NoPointView from '../view/no-point-view.js';
 
-export default class BoardPresenter {
-  eventListComponent = new EventListView();
+export default class TripEventsPresenter {
+  #pointsList = null;
+  #tripContainer = null;
+  #pointsModel = null;
+  #boardPoints = null;
+  #destinations = null;
+  #offers = null;
 
-  constructor({ container }) {
-    this.container = container;
+  constructor(tripContainer) {
+    this.#pointsList = new PointsView();
+    this.#tripContainer = tripContainer;
   }
 
-  init() {
-    render(new SortView(), this.container);
-    render(this.eventListComponent, this.container);
+  init(pointsModel) {
+    this.#pointsModel = pointsModel;
+    this.#boardPoints = [...this.#pointsModel.points];
+    this.#destinations = [...this.#pointsModel.destinations];
+    this.#offers = [...this.#pointsModel.offers];
 
-    render(new EditPointView, this.eventListComponent.getElement());
-
-    for (let i = 0; i < 3; i++) {
-      render(new PointView, this.eventListComponent.getElement());
+    if (this.#boardPoints.length === 0) {
+      render(new NoPointView(), this.#tripContainer);
+    }
+    else {
+      render(new SortingView(), this.#tripContainer);
+      render(this.#pointsList, this.#tripContainer);
+      for (const point of this.#boardPoints){
+        this.#renderPoint(point);
+      }
     }
   }
+
+  #renderPoint = (point) => {
+    const previewPointComponent = new PreviewPointView(point, this.#destinations, this.#offers);
+    const editingPointComponent = new EditingPointView(point, this.#destinations, this.#offers);
+
+    const replacePreviewPointToEditingPoint = () => {
+      replace(editingPointComponent, previewPointComponent);
+    };
+
+    const replaceEditingPointToPreviewPoint = () => {
+      replace(previewPointComponent, editingPointComponent);
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceEditingPointToPreviewPoint();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+
+    previewPointComponent.setEditClickHandler(() => {
+      replacePreviewPointToEditingPoint();
+      document.addEventListener('keydown', onEscKeyDown);
+    });
+
+    editingPointComponent.setPreviewClickHandler(() => {
+      replaceEditingPointToPreviewPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+
+    editingPointComponent.setFormSubmitHandler(() => {
+      replaceEditingPointToPreviewPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+
+    render(previewPointComponent, this.#pointsList.element);
+  };
 }
